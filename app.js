2e920eaf23646dc0f39cd2b3a4fd0c12e77ebd57
wlcharlie <wl00606352@gmail.com>
2021-05-01 17:58:56 +0800

    feat: add mongoose and schema settings

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express')
-const app = express()
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 const restaurantList = require('./restaurant.json').results
+
+const app = express()
 const port = 3000
+
+mongoose.connect('mongodb://localhost/restaurant-list', {
+  useNewUrlParser: true, useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!!')
+})
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
---
+++ models/restaurant.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const restaurantSchema = new Schema({
+  id: { type: Number, required: true },
+  name: { type: String, required: true },
+  name_en: { type: String },
+  category: { type: String, required: true },
+  image: { type: String, required: true },
+  location: { type: String, required: true },
+  phone: { type: String },
+  google_map: { type: String },
+  rating: { type: Number },
+  description: { type: String, required: true },
+})
+
+module.exports = mongoose.model('Restaurant', restaurantSchema)
