5d7f4a16aae74eab480f443f84da5b0e4d6889e5
wlcharlie <wl00606352@gmail.com>
2021-05-06 01:39:16 +0800

    refactor: mongodb mongoose

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const Restaurant = require('./models/restaurant')
 const methodOverride = require('method-override')
 
 const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
 const port = 3000
-
-mongoose.connect('mongodb://localhost/restaurant-list', {
-  useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false
-})
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!!')
-})
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,14 +1,7 @@
-const mongoose = require('mongoose')
+const db = require('../../config/mongoose')
+
 const Restaurant = require('../restaurant')
 const restaurantList = require('../../restaurant.json').results
-
-mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mondgodb error!')
-})
 
 db.once('open', () => {
   console.log('mongodb connected!')
