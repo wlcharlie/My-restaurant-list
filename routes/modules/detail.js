25e4b963169adf48568751c6daef80b8a3092e4f
wlcharlie <wl00606352@gmail.com>
2021-05-09 22:20:58 +0800

    refactor the create and using uuid4

--- routes/modules/detail.js
+++ routes/modules/detail.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const router = express.Router()
+const uuid4 = require('uuid4')
 
 const Restaurant = require('../../models/restaurant')
 
@@ -12,78 +13,53 @@ router.get('/new', (req, res) => {
 
 router.post('/', (req, res) => {
   const info = req.body
+  let id = parseInt(uuid4(), 24)
 
-  let id = Math.floor(Math.random() * 100000 + 1)
-  const check_ID = []
-  let checking = false
-
-  Restaurant.find({})
-    .lean()
-    .then(restaurants => restaurants.forEach(a => check_ID.push(a)))
-    .then(checking = true)
-    .catch(error => console.log(error))
-
-  while (checking) {
-    if (check_ID.some(ele => ele === id)) {
-      id = Math.floor(Math.random() * 100000 + 1)
-    } else {
-      checking = false
-      if (!info.image) {
-        info.image = "https://images.unsplash.com/photo-1619526882897-94e6516aff74"
-      }
-      if (!info.google_map) {
-        info.google_map = `https://www.google.com.tw/maps/search/${info.name}/`
-      }
-      Restaurant.create({
-        id,
-        name: info.name,
-        name_en: info.name_en,
-        category: info.category,
-        image: info.image,
-        location: info.location,
-        phone: info.phone,
-        google_map: info.google_map,
-        rating: info.rating,
-        description: info.description
-      })
-        .then(() => res.redirect('/'))
-        .catch(error => console.log(error))
-    }
+  if (!info.image) {
+    info.image = "https://images.unsplash.com/photo-1619526882897-94e6516aff74"
   }
+  if (!info.google_map) {
+    info.google_map = `https://www.google.com.tw/maps/search/${info.name}/`
+  }
+
+  Restaurant.create({
+    id,
+    name: info.name,
+    name_en: info.name_en,
+    category: info.category,
+    image: info.image,
+    location: info.location,
+    phone: info.phone,
+    google_map: info.google_map,
+    rating: info.rating,
+    description: info.description
+  })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 // view specific restaurants
 router.get('/:id', (req, res) => {
   const id = Number(req.params.id)
-
-  Restaurant.find({ id })
+  Restaurant.findOne({ id })
     .lean()
-    .then(findRestaurant => {
-      let theRestaurant = {}
-      theRestaurant = findRestaurant[0]
-      console.log(theRestaurant)
-      res.render('show', { theRestaurant })
-    })
+    .then(theRestaurant => res.render('show', { theRestaurant }))
     .catch(error => console.log(error))
 })
 
 // to edit page
 router.get('/:id/edit', (req, res) => {
   const id = Number(req.params.id)
-  Restaurant.find({ id })
+  Restaurant.findOne({ id })
     .lean()
-    .then(findRestaurant => {
-      let theRestaurant = {}
-      theRestaurant = findRestaurant[0]
-      res.render('edit', { theRestaurant })
-    })
+    .then(theRestaurant => res.render('edit', { theRestaurant }))
     .catch(error => console.log(error))
 })
 
+// renew the page from edit
 router.put('/:id', (req, res) => {
   const id = Number(req.params.id)
   const info = req.body
-
 
   if (!info.image) {
     info.image = "https://images.unsplash.com/photo-1619526882897-94e6516aff74"
